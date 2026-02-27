c711d27467a8963ba5c8ebee31b995517b071a72
jsaneto <j.netoass@gmail.com>
2026-02-27 20:59:48 -0300

    vpc-endpoint: criando o vpc-endpoint e sg 2

--- network.ts
+++ network.ts
@@ -2,8 +2,11 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
 export const createSecurityGroup = (vpcId: pulumi.Input<string>, nameSuffix: string) => {
-    return new aws.ec2.SecurityGroup(`sg-${nameSuffix}`, {
+    // Mudamos de `sg-${nameSuffix}` para algo como `sec-group-${nameSuffix}`
+    return new aws.ec2.SecurityGroup(`sec-group-${nameSuffix}`, {
         vpcId: vpcId,
+        // O campo 'name' também não pode começar com sg-
+        name: `security-group-${nameSuffix}`, 
         description: `Security Group para ${nameSuffix}`,
         ingress: [
             { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
@@ -12,7 +15,6 @@ export const createSecurityGroup = (vpcId: pulumi.Input<string>, nameSuffix: str
             { protocol: "tcp", fromPort: 5432, toPort: 5432, cidrBlocks: ["0.0.0.0/0"] },
             { protocol: "tcp", fromPort: 6379, toPort: 6379, cidrBlocks: ["0.0.0.0/0"] },
             { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"], description: "HTTPS para Endpoints" },
-            // Permite trafego interno entre as redes
             { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["10.0.0.0/16", "172.31.0.0/16"], description: "Trafego interno VPCs" },
         ],
         egress: [{ protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] }],
